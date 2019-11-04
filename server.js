8665a47c35895c1106046d6695750221652e74b7
Eugene Lee <eug703@gmail.com>
2019-11-04 00:28:13 -0500

    learning

--- server.js
+++ server.js
@@ -2,3 +2,14 @@ var express = require("express");
 var path = require("path");
 
 var app = express();
+var Port = process.env.PORT || 3030
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+require("./app/routes/apiRoutes")(app);
+require("./app/routes/htmlRoutes")(app);
+
+app.listen(Port, function(){
+    console.log(`App is listening on Port ${Port}`)
+})
