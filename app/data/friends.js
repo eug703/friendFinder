792df735c81db186f4bfe2d5f289a7e988fa8467
Eugene Lee <eug703@gmail.com>
2019-11-04 00:40:49 -0500

    friends added

---
+++ app/data/friends.js
@@ -0,0 +1,69 @@
+var friends = [
+    {
+        name: "Fry",
+        photo: "https://www.syfy.com/sites/syfy/files/cast_futurama_phillipjfry.jpg",
+        score : [
+            1,
+            1,
+            3,
+            4,
+            5,
+            2,
+            4,
+            2,
+            4,
+            4
+        ]
+    },
+    {
+        name: "Bender",
+        photo: "https://vignette.wikia.nocookie.net/villains/images/8/8f/The_Bender.jpg/revision/latest?cb=20180701200006",
+        score:[
+            5,
+            3,
+            5,
+            1,
+            4,
+            5,
+            1,
+            3,
+            1,
+            5,
+        ]
+    },
+    {
+        name : "Leela",
+        photo : "https://www.syfy.com/sites/syfy/files/styles/1200x680/public/cast_futurama_turangaleela_0.jpg",
+        score: [
+            4,
+            5,
+            5,
+            3,
+            2,
+            3,
+            5,
+            4,
+            5,
+            3
+        ]
+    },
+    {
+        name: "Professor",
+        photo: "https://www.syfy.com/sites/syfy/files/cast_futurama_proffarnsworth.jpg",
+        score: [
+            5,
+            1,
+            5,
+            1,
+            5,
+            2,
+            2,
+            5,
+            1,
+            3
+        ]
+    }
+]
+
+
+module.exports = friends
